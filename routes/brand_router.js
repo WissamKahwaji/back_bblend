41a3b48855f9432988c9711b710d376cb8dc5a3b
WissamKahwaji <kahwajiwisam@gmail.com>
2023-12-23 14:09:49 +0400

    dsa

--- routes/brand_router.js
+++ routes/brand_router.js
@@ -1,14 +1,10 @@
-import express from 'express';
-import { addBrandData, getBrandData } from '../controllers/brand_ctrl.js';
-
-
-
+import express from "express";
+import { addBrandData, getBrandData } from "../controllers/brand_ctrl.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
-
-router.get('/', getBrandData);
-router.post('/add', addBrandData);
-
+router.get("/", getBrandData);
+router.post("/add", auth, addBrandData);
 
 export default router;
